d08cca7e796c705048e38c4f2f70642fad17e8de
Ayush Jain <ayushjain2205@gmail.com>
2023-03-31 23:26:41 +0530

    Install daisy UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
 import { useState } from "react";
 import "./App.css";
-import GitHubLogin from "react-github-login";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const onSuccess = (response) => console.log(response);
-  const onFailure = (response) => console.error(response);
-
   return (
     <div className="App">
-      <GitHubLogin
-        clientId="c0793b9cdb31537dc710"
-        onSuccess={onSuccess}
-        onFailure={onFailure}
-      />
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="badge badge-accent">Test daisy</div>
     </div>
   );
 }
